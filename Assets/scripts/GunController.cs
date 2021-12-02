963dd0848e18b73c808a03874b91ff7551406b4a
pythonbishop <orangecamels244@gmail.com>
2021-12-02 12:45:29 -0500

    update

--- Assets/scripts/GunController.cs
+++ Assets/scripts/GunController.cs
@@ -5,48 +5,46 @@ using UnityEngine;
 public class GunController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float angle = 0;
-    public float trim;
+    public float angle;
     public float gunLength;
     public Vector3 bulletSpawn;
     public Vector3 offsetRight;
     public Vector3 offsetLeft;
+    public Vector3 gunToMouse;
+    Camera camera;
+    Vector3 mouseWorldPos;
     SpriteRenderer spriteRenderer;
+
     void Start()
     {
+        camera = Camera.main;
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.sortingLayerName = "character";
     }
     // Update is called once per frame
     void Update()
     {
+        mouseWorldPos = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition[0], Input.mousePosition[1]));
+        gunToMouse = new Vector3(mouseWorldPos.x - transform.position[0], mouseWorldPos.y - transform.position[1]);
+        angle = Vector3.Angle(Vector3.right, gunToMouse);
         
-        //gun face right
+        bulletSpawn = transform.position + Vector3.Normalize(gunToMouse) * gunLength;
+        transform.SetPositionAndRotation(transform.position, Quaternion.AngleAxis(angle, Vector3.forward));
+        
         if ((int)angle < 90 & (int)angle > -90)
         {
-        //angle from gun to mouse
-        angle = Mathf.Atan2(Input.mousePosition[1] - Screen.height/2 - offsetRight.y * trim, Input.mousePosition[0] - Screen.width/2 - offsetRight.x * trim) * Mathf.Rad2Deg;
-
+        //gun face right
         spriteRenderer.flipX = false;
         spriteRenderer.sortingOrder = 2;
         transform.localPosition = offsetRight;
-        transform.SetPositionAndRotation(transform.position, Quaternion.AngleAxis(angle, Vector3.forward));
-        bulletSpawn = transform.position + Vector3.Normalize(new Vector3(Input.mousePosition[0] - Screen.width/2 - offsetRight.x * trim, Input.mousePosition[1] - Screen.height/2 - offsetRight.y * trim)) * gunLength;
         }
 
-        //gun face left
         else if ((int)angle > 90 || (int)angle < -90)
         {
-        //angle from gun to mouse
-        angle = Mathf.Atan2(Input.mousePosition[1] - Screen.height/2 - offsetLeft.y * trim, Input.mousePosition[0] - Screen.width/2 - offsetLeft.x * trim) * Mathf.Rad2Deg;
-
+        //gun face left
         spriteRenderer.flipX = true;
         spriteRenderer.sortingOrder = 0;
         transform.localPosition = offsetLeft;
-        transform.SetPositionAndRotation(transform.position, Quaternion.AngleAxis(angle-180, Vector3.forward));
-        bulletSpawn = transform.position + Vector3.Normalize(new Vector3(Input.mousePosition[0] - Screen.width/2 - offsetLeft.x * trim, Input.mousePosition[1] - Screen.height/2 - offsetLeft.y * trim)) * gunLength;
         }
-
-        angle = Mathf.Atan2(Input.mousePosition[1] - Screen.height/2, Input.mousePosition[0] - Screen.width/2) * Mathf.Rad2Deg;
     }
 }
